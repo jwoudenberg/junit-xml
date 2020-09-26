e389ee5fb9ae3d865bbc4588a93cc3f87aaab5e9
Jasper Woudenberg <mail@jasperwoudenberg.com>
2020-09-26 10:30:05 +0100

    Improve release script

--- release.sh
+++ release.sh
@@ -1,6 +1,19 @@
 #!/usr/bin/env bash
 
+set -euxo pipefail
+
+name=$(cat package.yaml | grep name: | awk '{print $2}')
+version=$(cat package.yaml | grep version: | awk '{print $2}')
+bundle="$name-$version.tar.gz"
+
+# check copyright year is current year
+grep "Copyright (c) $(date +'%Y')" < LICENSE
+
+# check github release tag exists
+git fetch --tags
+git tag -l --points-at HEAD | grep "^$version$"
+
 hpack
-cabal sdist -o - > bundle.tar.gz
-cabal upload --publish bundle.tar.gz
+cabal sdist -o - > "$bundle"
+cabal upload --publish "$bundle"
 cabal upload -d --publish
