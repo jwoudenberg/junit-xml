5f250d9fbb89c23dfa19e9c3215cdeb91e2f916d
Jasper Woudenberg <mail@jasperwoudenberg.com>
2023-09-10 10:39:17 +0200

    Support tasty 1.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,10 @@
+# 0.1.0.3
+
+- Expand version bounds to include tasty 1.5.
+
 # 0.1.0.2
 
-- Expand version bounds to include tasy 1.4.
+- Expand version bounds to include tasty 1.4.
 
 # 0.1.0.1
 
--- release.sh
+++ release.sh
@@ -2,9 +2,21 @@
 
 set -euxo pipefail
 
+function fail {
+  echo "$1"
+  exit 1
+}
+
 name=$(grep name: < package.yaml | awk '{print $2}')
 version=$(grep version: < package.yaml | awk '{print $2}')
 bundle="$name-$version.tar.gz"
+
+hpack
+
+# check repository does not contain uncomitted changes
+if git status --porcelain | grep . ; then
+  fail "Stash any changes before starting the release script."
+fi
 
 # check changelog contains an entry for this version
 grep "^# $version$" < CHANGELOG.md
@@ -17,7 +29,6 @@ grep "Copyright (c) $(date +'%Y')" < LICENSE
 git fetch --tags
 git tag -l --points-at HEAD | grep "^$version$"
 
-hpack
 cabal sdist -o - > "$bundle"
 cabal upload --publish "$bundle"
 cabal upload -d --publish
